c188ee8840a2e8567f13c5a661eb35143bd3bb4c
jzdynamo <jzdynamo@gmail.com>
2016-12-25 23:40:30 +0800

    [Webpack] [Package.json] Basic webpack + React + Babel settings.
    Include:
    1. node_modules: babel, react, webpack, redux
    2. basic package.json
    3. webpack.config.js
    4. Basic web app entry and App.jsx.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+"use strict";
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || "8080";
+
+module.exports = {
+	entry: [
+		'./src/client/web/main.jsx'
+	],
+	output: {
+		path: './build/web',
+		filename: 'bundle.js'
+	},
+	module: {
+    loaders:[
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      }
+    ]
+	},
+	devServer: {
+		contentBase: "./build/web",
+		port: PORT,
+		host: HOST
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/client/web/template.html'
+		})
+	]
+};
