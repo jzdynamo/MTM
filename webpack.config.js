b1b6b35524a814382e0111eab9c148092457e1bb
jzdynamo <jzdynamo@gmail.com>
2016-12-26 22:01:35 +0800

    [Babel] Support decorators in webpack and babel.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,11 @@ module.exports = {
       {
         test: /\.js[x]?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader?presets[]=es2015&presets[]=react&presets[]=stage-0'
+        loader: 'babel',
+				query: {
+					plugins: ['transform-decorators-legacy' ],
+    			presets: ['es2015', 'stage-0', 'react']
+				}
       },
       {
         test: /\.css$/,
